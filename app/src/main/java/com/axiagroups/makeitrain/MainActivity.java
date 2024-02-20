45937ed0a89f6e68f5191e92118b4f450f9e54ea
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-02-20 13:47:46 +0600

    Tost done

--- app/src/main/java/com/axiagroups/makeitrain/MainActivity.java
+++ app/src/main/java/com/axiagroups/makeitrain/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -24,6 +25,17 @@ public class MainActivity extends AppCompatActivity {
         moneyValue = findViewById(R.id.moneyValue);
         showInfo = findViewById(R.id.buttonShowInfo);
         makeItRain = findViewById(R.id.buttonMakeItRain);
+
+        showInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence text = "Hello You have " + String.valueOf(moneyCounter) + " USD!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(MainActivity.this, text, duration);
+                toast.show();
+            }
+        });
     }
 
     public void showInfo(View view) {
